6919f4fc03eb89e024194e3b2645fc20e749d37a
rokipdj88 <candraxr4@gmail.com>
2025-07-28 13:52:19 +0700

    Create erc20-abi.js

---
+++ erc20-abi.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [{ name: "", type: "string" }],
+    type: "function"
+  }
+];
